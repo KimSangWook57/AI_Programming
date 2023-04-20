4e1fca5597151f19dbc1a7f8f48bb943c543b82c
KimSangWook57 <asdewrp@naver.com>
2023-04-20 09:49:41 +0900

    소스코드 다운로드

--- src/tsp.py
+++ src/tsp.py
@@ -1,70 +1,91 @@
-import math
 import random
-# 내가 직접 작성하기
-# 읽기 좋게 쓰기
+import math
 
-# TODO: 인접행렬 그래프 만들고 다시 해보기
+eval_count = 0
+
+
 def create_problem(filename):
     f = open(filename, "r")
     num_cities = int(f.readline())
-
     locations = []
     for line in f.readlines():
         locations.append(eval(line))
-    
     f.close()
-    table = create_distance_table(num_cities, locations)
+    table = calc_distance_table(num_cities, locations)
     return num_cities, locations, table
 
-def create_distance_table(num_cities, locations):
-    # 거리 계산 필요
-    # 맨하튼 => 격자(grid world)
-    # 유클리드 거리(점 2개 필요?)
-    
+
+def calc_distance_table(num_cities, locations):
     table = []
     for i in range(num_cities):
-        # 재귀적인 코드
         line = []
         for k in range(num_cities):
-            distance = math.sqrt(((locations[i][0] - locations[k][0]) ** 2) + (locations[i][1] - locations[k][1]) ** 2)
-            # distance = abs(locations[i][0] - locations[k][0]) + abs(locations[i][1] - locations[k][1]) # 맨하튼 거리
+            distance = math.sqrt(
+                (locations[i][0] - locations[k][0]) ** 2
+                + (locations[i][1] - locations[k][1]) ** 2
+            )
             line.append(distance)
         table.append(line)
     return table
 
+
 def random_init(p):
-    # 결과 shuffle!
-    # 무작위 좌표를 만들어내야 한다.
     n = p[0]
     init = list(range(n))
     random.shuffle(init)
     return init
 
+
 def evaluate(current, p):
+    global eval_count
+    eval_count += 1
     cost = 0
     num_cities, locations, table = p
-    # 인덱스를 써야 한다.
     for i in range(num_cities):
-        cost += table[current[i]][current[i-1]]
+        cost += table[current[i]][current[i - 1]]
     return cost
+
+
+def inversion(current, i, j):
+    current_copy = current[:]
+    while i < j:
+        current_copy[i], current_copy[j] = current_copy[j], current_copy[i]
+        i += 1
+        j -= 1
+    return current_copy
+
 
 def describe_problem(p):
     print()
     n = p[0]
-    print(f"Number of cities : {n}")
+    print(f"Number of cities: {n}")
+    print(f"Locations:")
     locations = p[1]
-    table = p[2]
     for i in range(n):
-        print(f"{i}")
+        print(f"{str(locations[i]):>12}", end="")
         if i % 5 == 4:
             print()
-        
+
+
+def display_result(solution, minimum):
+    print()
+    print("Best order of visits:")
+    ten_per_row(solution)  # Print 10 cities per row
+    print(f"Minimum tour cost: {round(minimum):,}")
+    print()
+    print(f"Total number of evaluations: {eval_count:,}")
+
+
+def ten_per_row(solution):
+    for i in range(len(solution)):
+        print(f"{solution[i]:>5}", end="")
+        if i % 10 == 9:
+            print()
+
+
 if __name__ == "__main__":
-    # 문제 인스턴스 생성
     p = create_problem("./data/tsp30.txt")
-    # print(p)
-    # describe_problem(p)
-    # 초기 솔루션 생성
-    init = random_init(p)
-    # 솔루션에 대한 문제평가
-    print(evaluate(init, p))
+    solution = random_init(p)
+    minimum = evaluate(solution, p)
+    describe_problem(p)
+    display_result(solution, minimum)
